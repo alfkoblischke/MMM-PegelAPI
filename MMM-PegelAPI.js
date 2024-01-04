1d31bf4f816d2a7e1131cc4ef62791c489bdcdf8
alfkoblischke <alf@koblischke.com>
2024-01-04 14:44:11 +0100

    Update MMM-PegelAPI.js

--- MMM-PegelAPI.js
+++ MMM-PegelAPI.js
@@ -1,58 +1,92 @@
 Module.register("MMM-PegelAPI", {
-    defaults: {
-        category: "Programming",
-        fetchInterval: 10 * 1000
-    },
-    getStyles() {
-        return [
-            this.file('style.css')
-        ]
-    },
-    pegel: null,
-    notificationReceived(notification, payload, sender) {
-        if (notification === 'MODULE_DOM_CREATED') {
-            this.getPegel();
-            setInterval(() => {
-                this.getPegel()
-            }, this.config.fetchInterval);
-        }
-    },
-    getDom() {
-        const wrapper = document.createElement("div");
+  defaults: {    
+    updateInterval: 600000,
+    pegelName: {
+      "a6ee8177-107b-47dd-bcfd-30960ccc6e9c": "Köln"      
+    },    
+  },
 
-        if(this.pegel === null) return wrapper;
+  getStyles: function () {
+    return ["style.css"];
+  },
 
-        this.setupHTMLStructure(wrapper);
+  // Override start method
+  start: function () {
+    this.loaded = false;
+    this.prices = {};
+    this.url = `https://pegelonline.wsv.de/webservices/rest-api/v2/stations/${Object.keys(this.config.pegelName)}/W/measurements.json`;
+    this.getData();
+    setInterval(() => {
+      this.getData();
+    }, this.config.updateInterval);
+  },
 
-        return wrapper;
-    },
-    setupHTMLStructure(wrapper) {
-        if (this.pegel.type === 'single') {
+  getData: async function () {
+    try {
+      const response = await fetch(this.url);
+      const data = await response.json();
 
-            const pegel = document.createElement("h1");
-            pegel.className = "bright medium light fadeInPegel";
-            pegel.innerHTML = this.pegel.pegel;
-            wrapper.appendChild(pegel);
-
-        } else if (this.pegel.type === 'twopart') {
-
-            const setup = document.createElement("h1");
-            setup.className = "bright medium light no-wrap fadeInPegel";
-            setup.innerHTML = this.pegel.setup;
-            wrapper.appendChild(setup);
-
-            const punchline = document.createElement("h2");
-            punchline.className = "bright small light fadeInPunchline";
-            punchline.innerHTML = this.pegel.delivery;
-            wrapper.appendChild(punchline);
-        }
-    },
-    getPegel() {
-        fetch(`https://pegelonline.wsv.de/webservices/rest-api/v2/stations/a6ee8177-107b-47dd-bcfd-30960ccc6e9c/W/measurements.json`).then((response) => {
-            response.json().then((pegel) => {
-                this.pegel = pegel;
-                this.updateDom();
-            });
-        });
+      if (data.ok) {
+        this.letzterPegel = data[data.length-1]['value'];
+        this.letzterPegelTime = data[data.length-1]['timestamp'];
+        this.loaded = true;
+        this.updateDom();
+      } else {
+        Log.error("Fehler beim Abrufen der Daten von Tankerkoenig API.");
+      }
+    } catch (error) {
+      Log.error(`Fehler beim Abrufen der Daten von Tankerkoenig API: ${error}`);
     }
+  },
+
+  getHeader: function () {
+    return "Meine Pegelstände";
+  },
+
+  getDom: function () {
+    var wrapper = document.createElement("table");
+    wrapper.className = "small tanker-table";
+
+    if (!this.loaded) {
+      wrapper.innerHTML = "Lade Pegelstände...";
+      wrapper.className = "dimmed light";
+      return wrapper;
+    }
+
+    // Header Row
+    var headerRow = document.createElement("tr");
+    var stationNameHeader = document.createElement("th");
+    stationNameHeader.innerHTML = "Ort";
+    headerRow.appendChild(stationNameHeader);
+    var statusHeader = document.createElement("th");
+    statusHeader.innerHTML = "Status";
+    headerRow.appendChild(statusHeader);
+    wrapper.appendChild(headerRow);
+
+    // Data Rows
+    for (var pegel in this.pegel) {
+      var pegelData = this.pegel[pegelId];
+      var row = document.createElement("tr");
+
+      // Station Name
+      var stationName = document.createElement("td");
+      stationName.innerHTML = this.config.pegelNames[pegelId];
+      row.appendChild(pegelnName);
+
+      // Status (open or closed)
+      var status = document.createElement("td");
+      status.innerHTML = stationData.status === "open" ? "geöffnet ✓" : "geschlossen ✗";
+      status.style.color = stationData.status === "open" ? "green" : "red";
+      row.appendChild(status);
+
+      // Fuel Prices
+     
+      row.appendChild(pegelValue);
+     
+
+      wrapper.appendChild(row);
+    }
+
+    return wrapper;
+  }
 });
