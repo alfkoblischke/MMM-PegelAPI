b58ecc1c3c37b85f363b8448abe7334dcfca0ed3
alfkoblischke <alf@koblischke.com>
2024-01-04 17:00:47 +0100

    Update MMM-PegelAPI.js

--- MMM-PegelAPI.js
+++ MMM-PegelAPI.js
@@ -107,8 +107,9 @@ Module.register("MMM-PegelAPI", {
 
     // Water Name
     var waterName = document.createElement("td");
-    waterName.innerHTML = this.stationWater;
-    row.appendChild(capitalize(waterName));
+    waterName.toLowerCase().innerHTML = this.stationWater;
+    
+    row.appendChild(waterName);
 
      // Pegel Km
     var pegelKm = document.createElement("td");
