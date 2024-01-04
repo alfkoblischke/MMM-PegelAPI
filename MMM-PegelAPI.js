a9506cebd32112f18583490c681e4796da4c39b3
alfkoblischke <alf@koblischke.com>
2024-01-04 18:22:55 +0100

    Update MMM-PegelAPI.js

--- MMM-PegelAPI.js
+++ MMM-PegelAPI.js
@@ -139,7 +139,7 @@ Module.register("MMM-PegelAPI", {
     var imageColumn = document.createElement("td");
     var pegelImage = document.createElement("img");
     pegelImage.src = this.image;      
-    consule.log(this.image);
+    console.log(this.image);
     console.log(pegelImage);
     imageColumn.innerHTML = pegelImage; 
     imagerow.appendChild(imageColumn);    
